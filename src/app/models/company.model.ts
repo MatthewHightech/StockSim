8fb8a26aeba4260657c3826ab20372a5a5686ae2
MatthewHightech <Smoose007@gmail.com>
2021-07-06 08:29:56 -0700

    initCompany data

--- src/app/models/company.model.ts
+++ src/app/models/company.model.ts
@@ -1,5 +1,5 @@
 export interface company {
   name: string
   bio: string
-  valueChange: number[] 
+  priceDiff: number[]
 }
--- src/app/services/companies.service.ts
+++ src/app/services/companies.service.ts
@@ -14,9 +14,39 @@ export class CompaniesService {
     private auth: AngularFireAuth
   ) {}
 
-  companies: company
+  companies: company[]
 
-  
+  async initCompanies() {
+    this.companies = [];
+    let company;
+
+    let size = await this.firestore.collection('companies').get().toPromise().then(res => {
+      return res.size
+    });
+
+    for (let i = 0; i < size; i++) {
+      company = await this.firestore.collection('students').doc<company>(i.toString())
+      .get()
+      .toPromise()
+      .then(res => {
+        return {
+          name: res.data().name,
+          bio: res.data().bio,
+          priceDiff: res.data().priceDiff
+        };
+      })
+      .catch(e => {
+        console.log("Connot get Company Data: ", e);
+        return null;
+      });
+      this.companies.push(company);
+    }
+
+    console.log("Company Data: ", this.companies);
+
+  }
+
+  // TODO: Setup super-admin page to add or edit tool data
 
 
 }
