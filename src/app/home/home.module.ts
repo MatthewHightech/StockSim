2a881fa7fd984a261d20989a71345304050e52f6
MatthewHightech <Smoose007@gmail.com>
2021-07-09 08:44:00 -0700

    Chartjs library added (must run npm i to add to local node modules)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ChartsModule
   ],
   declarations: [HomePage, PortfolioComponent]
 })
